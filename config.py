9fbfdbd2bcc04938bb54ab58857990877f2e0f27
InterStellarCellar <123860220+InterStellarCellar@users.noreply.github.com>
2026-04-03 11:51:28 +1100

    Updated config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+#DBF, QBF and CBF are all of size 100KB and use 3 hashes
+BLOOM_SIZE_BYTES = 100 * 1024   # 100KB
+BLOOM_NUM_HASHES = 3
 
+#backend server using TCP port No 55000
+SERVER_PORT = 55000
+
+#32-Byte EphID
+EPHID_SIZE = 32
+
+#broadcast these n shares @ 1 unique share per 3 seconds
+SHARE_BROADCAST_INTERVAL = 3   # seconds
+
+#each node stores at most 6 DBFs
+MAX_DBFS = 6
